bf8f6c63789ddc14976153202771ef746e70bac8
Averi <averi.sherman@gmail.com>
2022-08-31 19:58:59 -0400

    added hover and space between search and input

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+var input = document.querySelector('#inpt');
+var button = document.querySelector('#searchBtn');
+
+
+// input form event listeners/functions
+
+// fetch api
+// results
+
+// divs
+
+// function searchBar(event){
+//     event.preventDefault();
+//     console.log("test");
+// }
+// button.addEventListener('click', searchBar);

0d22c29cffb52e6c605e77ddf4ed67d60394cd61
Averi <averi.sherman@gmail.com>
2022-09-01 15:14:04 -0400

    javascript

--- assets/script.js
+++ assets/script.js
@@ -9,8 +9,46 @@ var button = document.querySelector('#searchBtn');
 
 // divs
 
-// function searchBar(event){
-//     event.preventDefault();
-//     console.log("test");
-// }
-// button.addEventListener('click', searchBar);
+function searchBar(event){
+    event.preventDefault();
+    console.log("test");
+// INPUT AND SEARCH BUTTON DISAPPEAR WHEN CLICKED
+    input.style.display= "none";
+    button.style.display= "none";
+
+
+
+}
+button.addEventListener('click', searchBar);
+
+var apiKey = 'M8SMVMpQtd5nLVLJvfbiAwx0lmXDa9dKkuCLRuq8';
+
+var apiUrl = 'https://developer.nps.gov/api/v1/parks?q=&api_key'+ apiKey;
+
+var box = document.querySelector("#display")
+var box2 = $("#box")
+console.log(box)
+
+
+        
+        fetch(apiUrl)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            console.log(data)
+            var h1el = document.createElement("h1")
+
+            h1el.textContent= data.title
+            h1el.style.color = "red"
+            h1el.style.margin= "20p "
+            box.append(h1el)
+
+            })
+
+
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
+}
+
+input.addEventListener("submit", handleSearchFormSubmit);

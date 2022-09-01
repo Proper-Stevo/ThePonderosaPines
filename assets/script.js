373485bc1d5cd4b4b192d441524a7cc9ea2dcd21
Averi <averi.sherman@gmail.com>
2022-09-01 15:51:22 -0400

    pulled from JM branch

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,44 @@
 var input = document.querySelector('#inpt');
 var button = document.querySelector('#searchBtn');
 
+var apiKey = '60tnQ2dzLAkLf73fsvhdfvhec1KAxFeawuS3TRTS';
 
+// 1st page, 2nd page
+var box = document.querySelector(".box")
+var box2 = $(".box")
+console.log (box)
+// click searchbar
+function searchBar(event){
+    event.preventDefault();
+    console.log(input.value);
+// INPUT AND SEARCH BUTTON DISAPPEAR WHEN CLICKED
+    input.style.display= "none";
+    button.style.display= "none";
+
+    var apiurl = `https://developer.nps.gov/api/v1/parks?q=${input.value}&api_key=${apiKey}`; 
+
+
+fetch(apiurl)
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data)
+    var h1El = document.createElement("h1")
+
+    h1El.textContent=data.data[0].description
+    h1El.style.color = "green"
+    h1El.style.margin = "20px"
+    box.append(h1El)
+    var contacts = data.data
+    for(var i = 0; i < contacts.length; i++){
+        var h2El = document.createElement("h2")
+        h2El.textContent = contacts[i].description
+        h2El.setAttribute("class", "description")
+        box.append(h2El)
+}
+})
+}
 // input form event listeners/functions
 
 // fetch api
@@ -9,46 +46,4 @@ var button = document.querySelector('#searchBtn');
 
 // divs
 
-function searchBar(event){
-    event.preventDefault();
-    console.log("test");
-// INPUT AND SEARCH BUTTON DISAPPEAR WHEN CLICKED
-    input.style.display= "none";
-    button.style.display= "none";
-
-
-
-}
 button.addEventListener('click', searchBar);
-
-var apiKey = 'M8SMVMpQtd5nLVLJvfbiAwx0lmXDa9dKkuCLRuq8';
-
-var apiUrl = 'https://developer.nps.gov/api/v1/parks?q=&api_key'+ apiKey;
-
-var box = document.querySelector("#display")
-var box2 = $("#box")
-console.log(box)
-
-
-        
-        fetch(apiUrl)
-        .then(function(response){
-            return response.json()
-        })
-        .then(function(data){
-            console.log(data)
-            var h1el = document.createElement("h1")
-
-            h1el.textContent= data.title
-            h1el.style.color = "red"
-            h1el.style.margin= "20p "
-            box.append(h1el)
-
-            })
-
-
-function handleSearchFormSubmit(event) {
-    event.preventDefault();
-}
-
-input.addEventListener("submit", handleSearchFormSubmit);

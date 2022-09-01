564df09f92e49b06d14250c73677f804926893d9
Josh Murray <joshmurray@Joshs-MBP.home>
2022-09-01 12:08:17 -0700

    updated and organized API

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 var input = document.querySelector('#inpt');
 var button = document.querySelector('#searchBtn');
 
-var apiKey = '60tnQ2dzLAkLf73fsvhdfvhec1KAxFeawuS3TRTS'
+var apiKey = '60tnQ2dzLAkLf73fsvhdfvhec1KAxFeawuS3TRTS';
 
-var apiurl = `https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${apiKey}`
+
 
 var box = document.querySelector(".box")
 var box2 = $(".box")
@@ -11,10 +11,11 @@ console.log (box)
 
 function searchBar(event){
     event.preventDefault();
-    // console.log("test");
+    console.log(input.value);
     input.style.display= "none";
     button.style.display= "none";
-}
+
+    var apiurl = `https://developer.nps.gov/api/v1/parks?q=${input.value}&api_key=${apiKey}`;
 
 fetch(apiurl)
 .then(function(response){
@@ -36,6 +37,7 @@ fetch(apiurl)
         box.append(h2El)
 }
 })
+}
 // input form event listeners/functions
 
 // fetch api

f909ca344868696c832b906158150ae88ac9f927
Josh Murray <joshmurray@Joshs-MBP.lan>
2022-09-02 11:04:37 -0700

    API bug fixes, image styling

--- assets/script.js
+++ assets/script.js
@@ -22,7 +22,7 @@ fetch(apiurl)
     return response.json()
 })
 .then(function(data){
-    console.log(data)
+    console.log(data.data[0])
     var h1El = document.createElement("h1")
     var h2El = document.createElement("h2")
     var imageEl = document.createElement("img")
@@ -32,17 +32,17 @@ fetch(apiurl)
     imageEl.setAttribute("class", "image")
     box.append(imageEl)
 
-    h1El.textContent=data.data[0].description
-    h1El.style.color = "white"
-    h1El.style.margin = "20px"
-    box.append(h1El)
+    // h1El.textContent=data.data[0].description
+    // h1El.style.color = "white"
+    // h1El.style.margin = "20px"
+    // box.append(h1El)
 
     pEl.textContent=data.data[0].operatingHours[0].description
     pEl.setAttribute("class", "operating")
     box.append(pEl)
 
     var contacts = data.data
-    for(var i = 0; i < contacts.length; i++){
+    for(var i = 0; i < 1; i++){
         var h2El = document.createElement("h2")
         h2El.textContent = contacts[i].description
         h2El.setAttribute("class", "description")
